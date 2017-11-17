63dfc9de25a487e7ce54fa30c41c1712c816401e
VictorRubio00 <33484706+VictorRubio00@users.noreply.github.com>
2017-11-17 10:15:28 +0100

    Add files via upload

---
+++ pythonavancat11.py
@@ -0,0 +1,15 @@
+#-*-coding: utf-8 -*-
+numero = int(input("Dime cuantas palabras tiene la linea: "))
+lista = []
+palabra = []
+def lista(palabra):
+	if numero < 1:
+		print("¡Imposible!")
+	else:
+		lista = []
+		for i in range(numero):
+			print("Dígame la palabra")
+			palabra = raw_input()
+			lista += [palabra]
+		print("La lista creada es:", lista)
+lista (palabra)
